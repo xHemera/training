696fbd7480ca3e573253d7c0a2a53ab5647bf2d5
xHemera <spleeth.dev@gmail.com>
2025-09-26 14:13:50 +0200

    size_t in gnl causing code to not compile in real situation

--- gnl/repaired/gnl.c
+++ gnl/repaired/gnl.c
@@ -13,7 +13,7 @@ char	*ft_strchr(char *s, int c)
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int i = 0;
+	size_t i = 0;
 	while (i < n)
 	{
 		((char *)dest)[i] = ((char *)src)[i];
